8ea4ac9c9d9c3bba67e7d3957e7b10ed9ea6f2a8
Liujie0012 <jayliumanagement@gmail.com>
2025-05-30 20:11:47 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,12 @@
+// JavaScript Documentvar 
+typed = new Typed('.element', {
+               	strings: [" NGUYỄN TÚ ANH "]
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:false,
+        		});
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+$('.counter').countUp();
+  AOS.init();
